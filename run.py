3e28d86d90b4dcd4fe6f125401c839b71ce8dd92
Arkady Zaytsev <me@ihare.ru>
2026-06-29 21:57:10 +0300

    prod 1.1

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 from app import app
-import webbrowser
-import threading
+# import webbrowser
+# import threading
 
 
-def open_browser():
-    webbrowser.open("http://127.0.0.1:5555")
+# def open_browser():
+#     webbrowser.open("http://127.0.0.1:5555")
 
 
 if __name__ == '__main__':
-    threading.Timer(1.0, open_browser).start()
+    # threading.Timer(1.0, open_browser).start()
     app.run(debug=False, port=5555)
